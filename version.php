57f8a7ee5d2d8ffdee886cc15dd2b69a5d44cf2d
valery <valery.fremaux@gmail.com>
2016-03-29 12:32:12 +0200

    fix version

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2015100802; // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012120300; // Requires this Moodle version
+$plugin->requires  = 2014110400; // Requires this Moodle version
 $plugin->component = 'report_learningtimecheck'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '2.7.0 (build 2015100800)';
-$plugin->dependencies = array('mod_learningtimecheck' => '2015042200', 'local_vflibs' => '2015122000');
+$plugin->release = '2.8.0 (build 2015100802)';
+$plugin->dependencies = array('mod_learningtimecheck' => '2015071300');
