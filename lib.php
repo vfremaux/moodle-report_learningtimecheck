1eaeb56867a58a425f18afca0325b2d17262b1d6
valery <valery.fremaux@gmail.com>
2018-04-26 17:38:14 +0200

    load pro version

--- lib.php
+++ lib.php
@@ -75,6 +75,90 @@ function report_learningtimecheck_page_type_list($pagetype, $parentcontext, $cur
 }
 
 /**
+ * Local functions for this module
+ *
+ * @package    report_learningtimecheck
+ * @category   report
+ * @author     Valery Fremaux (valery.fremaux@gmail.com)
+ * @version    moodle 2.x
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Tells wether a feature is supported or not. Gives back the
+ * implementation path where to fetch resources.
+ * @param string $feature a feature key to be tested.
+ */
+function report_learningtimecheck_supports_feature($feature) {
+    global $CFG;
+    static $supports;
+
+    $config = get_config('report_learningtimecheck');
+
+    if (!isset($supports)) {
+        $supports = array(
+            'pro' => array(
+                'format' => array('xls', 'csv', 'pdf', 'xml'),
+                'mode' => array('batch'),
+                'export' => array('ws'),
+                'emulate' => 'community',
+            ),
+            'community' => array(
+                'format' => array('xml', 'csv'),
+            ),
+        );
+        $prefer = array('format' => array(
+            'xml' => 'community',
+            'csv' => 'community'
+        ));
+    }
+
+    // Check existance of the 'pro' dir in plugin.
+    if (is_dir(__DIR__.'/pro')) {
+        if ($feature == 'emulate/community') {
+            return 'pro';
+        }
+        if (empty($config->emulatecommunity)) {
+            $versionkey = 'pro';
+        } else {
+            $versionkey = 'community';
+        }
+    } else {
+        $versionkey = 'community';
+    }
+
+    list($feat, $subfeat) = explode('/', $feature);
+
+    if (!array_key_exists($feat, $supports[$versionkey])) {
+        return false;
+    }
+
+    if (!in_array($subfeat, $supports[$versionkey][$feat])) {
+        return false;
+    }
+
+    // Special condition for pdf dependencies.
+    if (($feature == 'format/pdf') && !is_dir($CFG->dirroot.'/local/vflibs')) {
+        return false;
+    }
+
+    if (array_key_exists($feat, $supports['community'])) {
+        if (in_array($subfeat, $supports['community'][$feat])) {
+            // If community exists, default path points community code.
+            if (isset($prefer[$feat][$subfeat])) {
+                // Configuration tells which location to prefer if explicit.
+                $versionkey = $prefer[$feat][$subfeat];
+            } else {
+                $versionkey = 'community';
+            }
+        }
+    }
+
+    return $versionkey;
+}
+
+/**
  * Is current user allowed to access this report
  *
  * @private defined in lib.php for performance reasons
@@ -1121,8 +1205,8 @@ function report_learningtimecheck_user_results(&$user, &$globals, $useroptions) 
     if ($timechecklistmodules) {
         foreach ($timechecklistmodules as $courseid => $coursechecks) {
             if (!array_key_exists($courseid, $CCACHE)) {
-                $field = 'id,shortname,fullname,idnumber';
-                $CCACHE[$courseid] = $DB->get_record('course', array('id' => $courseid), $field);
+                $fields = 'id,shortname,fullname,idnumber,format';
+                $CCACHE[$courseid] = $DB->get_record('course', array('id' => $courseid), $fields);
             }
 
             // Course name.
